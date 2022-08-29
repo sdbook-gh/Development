27e5792ba4f7b5760cd82da319aa1c9cd1749d14
sdbook-gh <sdbook@yeah.net>
2022-08-29 14:04:38 +0800

    update qt

--- qt/timer.cpp
+++ qt/timer.cpp
@@ -1,82 +1,23 @@
 #include <QLayout>
-
+#include <QProgressBar>
+#include <QTimer>
 #include "timer.h"
 
 Timer::Timer(QWidget *parent)
     : QWidget(parent) {
-    auto *layout = new QHBoxLayout();
-    /*progressBar = new QProgressBar();
+    QHBoxLayout *layout = new QHBoxLayout();
+    progressBar = new QProgressBar();
     progressBar->setMinimum(0);
     progressBar->setMaximum(100);
-    _ls_lable = new QLabel(tr("Source"));
-    _ls_content = new QLineEdit;
-    //_ls_content->setMaximumWidth(50);
-    _ls_content->setText("");
-    _li_lable = new QLabel(tr("Info"));
-    _li_content = new QLineEdit;
-    //_li_content->setMaximumWidth(50);
-    _li_content->setText("");
-    _filter_button = new QPushButton(tr("确定"));
     layout->addWidget(progressBar);
-    layout->addWidget(_ls_lable);
-    layout->addWidget(_ls_content);
-    layout->addWidget(_li_lable);
-    layout->addWidget(_li_content);
-    layout->addWidget(_filter_button);
+    setLayout(layout);
     QTimer *timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &Timer::updateProgress);
-    timer->start(1000);*/
-
-    auto *qc_widget = new QWidget(this);
-    qc_widget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    qc_widget->setMaximumHeight(20);
-    auto *qc_layout = new QHBoxLayout(qc_widget);
-    _qc_label = new QLabel(tr("强制质检"));
-    _qc_type = new QComboBox;
-    QStringList ql;
-    ql.push_back(tr("是"));
-    ql.push_back(tr("否"));
-    ql.push_back(tr("全部"));
-    _qc_type->addItems(ql);
-    ql.clear();
-    _qc_type->setCurrentIndex(2);
-    _lt_lable = new QLabel(tr("Type"));
-    _lt_type = new QComboBox;
-    ql.push_back(tr("INFO"));
-    ql.push_back(tr("WARNING"));
-    ql.push_back(tr("ERROR"));
-    ql.push_back(tr("全部"));
-    _lt_type->addItems(ql);
-    ql.clear();
-    _lt_type->setCurrentIndex(3);
-    _ls_lable = new QLabel(tr("Source"));
-    _ls_content = new QLineEdit;
-    //_ls_content->setMaximumWidth(50);
-    _ls_content->setText("");
-    _li_lable = new QLabel(tr("Info"));
-    _li_content = new QLineEdit;
-    //_li_content->setMaximumWidth(50);
-    _li_content->setText("");
-    _filter_button = new QPushButton(tr("确定"));
-    qc_layout->addWidget(_qc_label);
-    qc_layout->addWidget(_qc_type);
-    qc_layout->addWidget(_lt_lable);
-    qc_layout->addWidget(_lt_type);
-    qc_layout->addWidget(_ls_lable);
-    qc_layout->addWidget(_ls_content);
-    qc_layout->addWidget(_li_lable);
-    qc_layout->addWidget(_li_content);
-    qc_layout->addWidget(_filter_button);
-    qc_layout->setAlignment(Qt::AlignCenter);
-    qc_layout->setMargin(0);
-    qc_layout->setContentsMargins(0, 0, 0, 0);
-    qc_widget->setLayout(qc_layout);
-    layout->addWidget(qc_widget);
-    setLayout(layout);
+    timer->start(1000);
     setWindowTitle(tr("Timer and progress"));
-    //resize(10, 10);
+    resize(200, 200);
 }
 
 void Timer::updateProgress() {
-    //progressBar->setValue(progressBar->value() + 1);
+    progressBar->setValue(progressBar->value() + 1);
 }
--- qt/timer.h
+++ qt/timer.h
@@ -2,11 +2,6 @@
 
 #include <QWidget>
 #include <QProgressBar>
-#include <QTimer>
-#include <QLabel>
-#include <QComboBox>
-#include <QLineEdit>
-#include <QPushButton>
 
 class Timer : public QWidget {
     Q_OBJECT
@@ -15,14 +10,5 @@ public:
 public slots:
     void updateProgress();
 private:
-    //QProgressBar *progressBar {nullptr};
-    QLabel *_qc_label {nullptr};
-    QComboBox *_qc_type {nullptr};
-    QLabel *_lt_lable {nullptr};
-    QComboBox *_lt_type {nullptr};
-    QLabel *_ls_lable {nullptr};
-    QLineEdit *_ls_content {nullptr};
-    QLabel *_li_lable {nullptr};
-    QLineEdit *_li_content {nullptr};
-    QPushButton *_filter_button {nullptr};
+    QProgressBar *progressBar {nullptr};
 };
