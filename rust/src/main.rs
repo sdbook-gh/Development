144d16ea7d1918f3629999d9eb90c2b9f036daa9
shenda01 <shenda01@baidu.com>
2022-11-26 18:31:52 +0800

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -238,6 +238,7 @@ fn main() {
         MyStruct::hello_macro();
 
         {
+            use std::collections::HashMap;
             #[derive(Clone, PartialEq, Debug)]
             enum Json {
                 Null,
@@ -247,6 +248,57 @@ fn main() {
                 Array(Vec<Json>),
                 Object(Box<HashMap<String, Json>>),
             }
+            impl From<bool> for Json {
+                fn from(b: bool) -> Json {
+                    Json::Boolean(b)
+                }
+            }
+            impl From<String> for Json {
+                fn from(s: String) -> Json {
+                    Json::String(s)
+                }
+            }
+            impl<'a> From<&'a str> for Json {
+                fn from(s: &'a str) -> Json {
+                    Json::String(s.to_string())
+                }
+            }
+            macro_rules! impl_from_num_for_json {
+                ( $( $t:ident )* ) => {
+                $(
+                impl From<$t> for Json {
+                    fn from(n: $t) -> Json {
+                        Json::Number(n as f64)
+                    }
+                }
+                )*
+                };
+            }
+            impl_from_num_for_json!(u8 i8 u16 i16 u32 i32 u64 i64 u128 i128 usize isize f32 f64);
+
+            macro_rules! json {
+                (null) => {
+                    Json::Null
+                };
+                ([ $( $element:tt),* ]) => {
+                    Json::Array(vec![ $( json!($element) ),* ])
+                };
+                ({ $( $key:tt : $value:tt ),* }) => {
+                    Json::Object(Box::new(vec![
+                    $( ($key.to_string(), json!($value)) ),*
+                    ].into_iter().collect()))
+                };
+                ( $other:tt ) => {
+                    Json::from($other) // 处理布尔/数字/字符串
+                };
+            }
+            assert_eq!(json!(null), Json::Null); // passes!
+            let width = 4.0;
+            let desc = json!({
+                "width": 1,
+                "height": 2
+            });
+            dbg!(desc);
         }
     }
     return;
