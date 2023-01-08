f0094f527896b1b902480305c937c000252a1370
shenda01 <shenda01@baidu.com>
2023-01-08 16:47:27 +0800

    Update main.rs

--- rust/src/os/src/main.rs
+++ rust/src/os/src/main.rs
@@ -1,25 +1,104 @@
-#![no_std] // <1>
-#![no_main] // <1>
-#![feature(core_intrinsics)] // <2>
+#![no_std]
+#![no_main]
+#![feature(core_intrinsics)]
+#![feature(lang_items)]
 
-use core::intrinsics; // <2>
-use core::panic::PanicInfo; // <3>
-
+#[allow(unused)]
 #[panic_handler]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
-    intrinsics::abort(); // <4>
+pub fn panic(info: &core::panic::PanicInfo) -> ! {
+    let mut cursor = Cursor {
+        position: 0,
+        foreground: Color::White,
+        background: Color::Red,
+    };
+    for _ in 0..(80 * 25) {
+        cursor.print(b" ");
+    }
+    cursor.position = 0;
+    use core::fmt::Write;
+    write!(cursor, "{}", info).unwrap();
+
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+enum Color {
+    Black = 0x0,
+    White = 0xF,
+    Blue = 0x1,
+    BrightBlue = 0x9,
+    Green = 0x2,
+    BrightGreen = 0xA,
+    Cyan = 0x3,
+    BrightCyan = 0xB,
+    Red = 0x4,
+    BrightRed = 0xC,
+    Magenta = 0x5,
+    BrightMagenta = 0xD,
+    Brown = 0x6,
+    Yellow = 0xE,
+    Gray = 0x7,
+    DarkGray = 0x8,
+}
+
+struct Cursor {
+    position: isize,
+    foreground: Color,
+    background: Color,
+}
+
+impl Cursor {
+    fn color(&self) -> u8 {
+        let fg = self.foreground as u8;
+        let bg = (self.background as u8) << 4;
+        fg | bg
+    }
+    fn print(&mut self, text: &[u8]) {
+        let color = self.color();
+
+        let framebuffer = 0xb8000 as *mut u8;
+
+        for &character in text {
+            unsafe {
+                framebuffer.offset(self.position).write_volatile(character);
+                framebuffer.offset(self.position + 1).write_volatile(color);
+            }
+            self.position += 2;
+        }
+    }
+}
+impl core::fmt::Write for Cursor {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.print(s.as_bytes());
+        Ok(())
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let framebuffer = 0xb8000 as *mut u8;
+    let text = b"Hello Rust OS!";
+    let mut cursor = Cursor {
+        position: 0,
+        foreground: Color::Red,
+        background: Color::White,
+    };
+    cursor.print(text);
 
-    unsafe {
-        framebuffer
-            .offset(1) // <5>
-            .write_volatile(0x30); // <6>
+    let mut i = 0;
+    while i < 100000000 {
+        i += 1;
     }
 
-    loop {}
+    panic!("help!");
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
