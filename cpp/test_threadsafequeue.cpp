5afced4ed04f7ed3ae58a81b0a897ebe47cb907c
shenda <sd02178891@alibaba-inc.com>
2025-05-09 18:14:12 +0800

    update

---
+++ cpp/test_threadsafequeue.cpp
@@ -0,0 +1,75 @@
+#ifndef FRAMEWORK_BASE_THREAD_SAFE_QUEUE_H_
+#define FRAMEWORK_BASE_THREAD_SAFE_QUEUE_H_
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <utility>
+
+namespace netaos {
+namespace framework {
+namespace base {
+
+template <typename T> class ThreadSafeQueue {
+public:
+  ThreadSafeQueue() {}
+  ThreadSafeQueue &operator=(const ThreadSafeQueue &other) = delete;
+  ThreadSafeQueue(const ThreadSafeQueue &other) = delete;
+
+  ~ThreadSafeQueue() { BreakAllWait(); }
+
+  void Enqueue(const T &element) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.emplace(element);
+    cv_.notify_one();
+  }
+
+  bool Dequeue(T *element) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queue_.empty()) {
+      return false;
+    }
+    *element = std::move(queue_.front());
+    queue_.pop();
+    return true;
+  }
+
+  bool WaitDequeue(T *element) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    cv_.wait(lock, [this]() { return break_all_wait_ || !queue_.empty(); });
+    if (break_all_wait_) {
+      return false;
+    }
+    *element = std::move(queue_.front());
+    queue_.pop();
+    return true;
+  }
+
+  typename std::queue<T>::size_type Size() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.size();
+  }
+
+  bool Empty() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.empty();
+  }
+
+  void BreakAllWait() {
+    break_all_wait_ = true;
+    cv_.notify_all();
+  }
+
+private:
+  volatile bool break_all_wait_ = false;
+  std::mutex mutex_;
+  std::queue<T> queue_;
+  std::condition_variable cv_;
+};
+
+} // namespace base
+} // namespace framework
+} // namespace netaos
+
+#endif // FRAMEWORK_BASE_THREAD_SAFE_QUEUE_H_
