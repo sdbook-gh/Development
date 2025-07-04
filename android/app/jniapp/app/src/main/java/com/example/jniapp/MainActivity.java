c3dd0bef65edbcb91b134c5d3b8efe26ba5b654a
unknown <sdbook@yeah.net>
2025-07-04 11:00:40 +0800

    update android app

--- android/app/jniapp/app/src/main/java/com/example/jniapp/MainActivity.java
+++ android/app/jniapp/app/src/main/java/com/example/jniapp/MainActivity.java
@@ -1,36 +1,43 @@
 package com.example.jniapp;
 
-import android.app.Activity;
-import android.content.res.AssetManager;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
-    private MyGLSurfaceView glSurfaceView;
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        glSurfaceView = new MyGLSurfaceView(this);
-        setContentView(glSurfaceView);
+        setContentView(R.layout.activity_main);
 
-        // 将 AssetManager 传递给 native 层
-        AssetManager assetManager = getAssets();
-        glSurfaceView.setAssetManager(assetManager);
-    }
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.onlyYou);
+        tv.setText("jni app");
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        glSurfaceView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        glSurfaceView.onPause();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "JNI call result is: " + stringFromJNI(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
