b30d5e3ce435ffaaf566a38bdd5d907fc89cff82
unknown <sdbook@yeah.net>
2025-08-22 10:46:06 +0800

    update test_link

--- test_link/test_app.cpp
+++ test_link/test_app.cpp
@@ -3,18 +3,24 @@
 
 #include "backward.hpp"
 
-// int func_a();
+#if defined(TEST_LIB_STATIC)
+int func_a();
+#elif defined(TEST_LIB_DYNAMIC)
 int func_so();
+#endif
 namespace backward {
-  backward::SignalHandling sh;
+backward::SignalHandling sh;
 }
 
 int main() {
-  // func_a();
+#if defined(TEST_LIB_STATIC)
+  func_a();
+#elif defined(TEST_LIB_DYNAMIC)
   func_so();
+#endif
   google::InitGoogleLogging("");
-  FLAGS_logtostderr = 1;   
-  google::SetStderrLogging(google::GLOG_INFO);  
+  FLAGS_logtostderr = 1;
+  google::SetStderrLogging(google::GLOG_INFO);
   LOG(ERROR) << "main";
   return 0;
 }
